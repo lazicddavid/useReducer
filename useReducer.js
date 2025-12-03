0620622f9392778ec76bc3b5f98dc317e40b1a0b
David Lazic <lazic.ddavid@gmail.com>
2025-12-03 21:28:44 +0100

    -price),   clearBtn: document.querySelector(.clear-btn), };

---
+++ useReducer.js
@@ -0,0 +1,5 @@
+const DOMelements = {
+  container: document.querySelector(".container"),
+  totalPrice: document.querySelector(".total-price"),
+  clearBtn: document.querySelector(".clear-btn"),
+};
